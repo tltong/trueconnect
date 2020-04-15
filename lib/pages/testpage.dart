6466fe3f35af7f705585941b16152e70cfbeae07
tltong <tltong@yahoo.co.uk>
2020-04-16 01:07:02 +0800

    wip

--- lib/pages/testpage.dart
+++ lib/pages/testpage.dart
@@ -5,12 +5,6 @@ import 'package:carousel_slider/carousel_slider.dart';
 import '../user.dart';
 import '../utils/image_util.dart';
 
-
-
-
-
-
-
 class TestPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -19,24 +13,16 @@ class TestPage extends StatefulWidget {
   }
 }
 
-
 class TestPageState extends State<TestPage>{
 
 Photos photos;
-List<Image> images;
+static List<Image> images;
 static List<String> imageLinks;
 
-static List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-];
 
 static List<T> map<T>(List list, Function handler) {
   List<T> result = [];
+ 
   for (var i = 0; i < list.length; i++) {
     result.add(handler(i, list[i]));
   }
@@ -48,25 +34,47 @@ static List<T> map<T>(List list, Function handler) {
 final Widget placeholder = Container(color: Colors.grey);
 
 List child;
+BuildContext contextcopy;
 
+Widget _Dialog(BuildContext context, Image displayImage) {
+    return new AlertDialog(
+      //title: const Text('About Pop up'),
+      content: new Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          displayImage,
+          //Image.asset('images/addphoto.png'),
+          
+        ],
+      )
+     
+    );
+}
 
 
 void initialiseChild(){
 
 child = map<Widget>(
-  imageLinks,
+  images,
   (index, i) {
     return 
     GestureDetector(
        onTap: () {
-              print('tapped' + index.toString());
+              showDialog(
+            context: contextcopy,
+            builder: (BuildContext contextcopy) => _Dialog(contextcopy,i),
+          );
+
             },
     child: Container(
       margin: EdgeInsets.all(5.0),
       child: ClipRRect(
         borderRadius: BorderRadius.all(Radius.circular(5.0)),
         child: Stack(children: <Widget>[
-          Image.network(i, fit: BoxFit.scaleDown, width: 1000.0),
+            i,
+         // Image.network(i, fit: BoxFit.scaleDown, width: 1000.0),
+
           Positioned(
             bottom: 0.0,
             left: 0.0,
@@ -96,9 +104,7 @@ child = map<Widget>(
 
 @override
   void initState() {
-    print('init state');
-    initialiseChild();
-    
+   
     photos = appData.currentUser.getPhotos();
     images = appData.currentUser.photos.getSelectedPhotos();
     imageLinks = new List<String>();
@@ -107,9 +113,11 @@ child = map<Widget>(
       String imgstring = ImageUtil.ExtractImageString(image);
       imageLinks.add(imgstring);
     }
-    print(imageLinks);
+  // print(imageLinks);
 
 
+
+initialiseChild();
     super.initState();
 
   }
@@ -122,6 +130,7 @@ child = map<Widget>(
 
  @override
   Widget build(BuildContext context) {
+    contextcopy=context;
 
  return Scaffold(
       appBar: AppBar(
@@ -140,9 +149,8 @@ child = map<Widget>(
       viewportFraction: 0.9,
       aspectRatio: 2.0,
     ),
-     
 
-       
+      
             
             
 
