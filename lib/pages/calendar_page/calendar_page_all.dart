bed50b0dd90d6c78ae1781b0a887f102287506be
tltong <tltong@yahoo.co.uk>
2020-08-24 01:18:41 +0800

    calendar page WIP

--- lib/pages/calendar_page/calendar_page_all.dart
+++ lib/pages/calendar_page/calendar_page_all.dart
@@ -5,8 +5,10 @@ import 'dart:math';
 import '../../data/venuedata/venuedata.dart';
 import '../../data/venuedata/venuedata_dao.dart';
 
-List<VenueData> vdataList;
+
 VenueDataDao vDataDao;
+//final _pairList = <String>[];
+  final _pairList = <String>[];
 
 
 class CalendarPageAll extends StatefulWidget {
@@ -16,13 +18,16 @@ class CalendarPageAll extends StatefulWidget {
 //CalendarPageAll(List<VenueData> inVdataList){
 CalendarPageAll(VenueDataDao inVDataDao){
 
+ // print('calendar_page_all constructor');
   vDataDao= inVDataDao;
+  _pairList.clear();
+ // vDataDao.resetIndex();
 
-  vdataList = vDataDao.retrieveVenueData(vDataDao.retrieveCount());
+ // vdataList = vDataDao.retrieveVenueData(vDataDao.retrieveCount());
 
-  print('calendar_page : vdataList length : ' + vdataList.length.toString());
 
-    
+
+   
 }
 
 @override
@@ -34,41 +39,50 @@ CalendarPageAll(VenueDataDao inVDataDao){
 }
 
 class _ItemFetcher {
-  final _count = vdataList.length;
-  final _itemsPerPage = 4;
+  final _count = vDataDao.count;
+  final _itemsPerPage = 3;
   int _currentPage = 0;
   int index=0;
-
-
 
   // *******
 
   // This async function simulates fetching results from Internet, etc.
 //  Future<List<WordPair>> fetch() async {
     Future<List<String>> fetch() async {
+ //   print ('calendar_page_all : fetch start1 index : '+ index.toString());
     
     var random = new Random();
     var ranIndex = random.nextInt(2);
+    List<VenueData> retrievedVdatalist;
 
    // final list = <WordPair>[];
    final list = <String>[];
     final n = min(_itemsPerPage, _count - _currentPage * _itemsPerPage);
-  //  print('fetch - n vallue ' + n.toString());
+ //  print('calendar page : fetch n value ' + n.toString());
+   // print('calendar page : vDataDao index ' + vDataDao.index.toString());
+    retrievedVdatalist = vDataDao.retrieveVenueData(n);
+  //  print('calendar page : retrievedVdatalist length ' + retrievedVdatalist.length.toString());
 
     // Uncomment the following line to see in real time now items are loaded lazily.
     // print('Now on page $_currentPage');
     await Future.delayed(Duration(seconds: 1), () {
-      //  print('index : ' + index.toString());
-      for (int i = 0; i < n; i++) {
-      //  list.add(WordPair.random());
-      list.add(vdataList[index].name.toString());
+     //   print ('calendar_page_all : fetch start2 index : '+ index.toString());
+      
+      
+      for (int i = 0; i < retrievedVdatalist.length; i++) {
+       // list.add(WordPair.random());
+    //  print('calendar page : i vallue ' + i.toString());
+    //  print('calendar page : index value : ' + index.toString());
+
+      list.add(retrievedVdatalist[i].name.toString());
       index++;
     
-        
+    
       }
     });
     _currentPage++;
-    print('list : ' + list.toString());
+    print('calendar_page_all : fetch list : ' + list.toString());
+  
     return list;
   }
 }
@@ -79,8 +93,6 @@ class CalendarPageAllState extends State<CalendarPageAll>  {
   // ********
 
 //  final _pairList = <WordPair>[];
-  final _pairList = <String>[];
-
 
 
   final _biggerFont = const TextStyle(fontSize: 40.0);
@@ -93,6 +105,12 @@ final PageStorageBucket bucket = PageStorageBucket();
 
 void initState() {
 
+ //   print('calendar_page_all : init');
+  //  print('calendar_page init : vdataList length : ' + vDataDao.retrieveCount().toString());
+  //print('calendar_page init : pairlist.length : ' + _pairList.length.toString());
+ 
+
+
     super.initState();
       _isLoading = true;
     _hasMore = true;
@@ -100,12 +118,25 @@ void initState() {
   }
 
 void _loadMore() {
-      print('loadmore');
+   
     _isLoading = true;
+  //  print('calendar page all : loadmore before fetching : _pairList length ' + _pairList.length.toString() );
+//    print('calendar page all : loadmore before fetching : vDataDao length ' + vDataDao.retrieveCount().toString() );
+
+    if (_pairList.length >= vDataDao.retrieveCount()){
+  
+          _isLoading = false;
+          _hasMore = false;
+        return;
+
+    }
 
     // ************
 
-    _itemFetcher.fetch().then((List<String> fetchedList) {
+      _itemFetcher.fetch().then((List<String> fetchedList) {
+
+    //   print('calendar page all : fetchedlist length ' + fetchedList.length.toString() );
+   
       if (fetchedList.isEmpty) {
         setState(() {
           _isLoading = false;
@@ -115,6 +146,10 @@ void _loadMore() {
         setState(() {
           _isLoading = false;
           _pairList.addAll(fetchedList);
+
+      //     print('calendar page all : loadmore after fetching : _pairList length ' + _pairList.length.toString() );
+   //         print('calendar page all : loadmore after fetching : vDataDao length ' + vDataDao.retrieveCount().toString() );
+
         });
       }
     });
@@ -123,24 +158,41 @@ void _loadMore() {
 
 @override
   void dispose() {
+//    print('calendar_page_all : dispose');
+   // print('calendar_page_all dipose : pairlist.length : ' + _pairList.length.toString());
 
+
+   // _pairList.clear();
     super.dispose();
   }
 
 @override
 Widget build(BuildContext context) {
 
+  
+
+
  return ListView.builder(
-   
+
       itemCount: _hasMore ? _pairList.length + 1 : _pairList.length,
       itemBuilder: (BuildContext context, int index) {
+      //print('calendar_page_all build : _pairList length : ' + _pairList.length.toString());
+
+       // print('calendar_page_all build : index : ' + index.toString());
         // Uncomment the following line to see in real time how ListView.builder works
-         print('ListView.builder is building index $index');
+       //  print('ListView.builder is building index $index');
         if (index >= _pairList.length) {
           // Don't trigger if one async loading is already under way
           if (!_isLoading) {
             _loadMore();
           }
+
+          if ( _isLoading == false && _hasMore == false){
+            return null;
+          }
+
+
+
           return Center(
             child: SizedBox(
               child: CircularProgressIndicator(),
--- lib/pages/calendar_page/calendar_page_tabs.dart
+++ lib/pages/calendar_page/calendar_page_tabs.dart
@@ -32,22 +32,91 @@ CalendarPagesTabs(){
     File file3 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
     Image image3 = Image.file(file3); 
     imagelist3.add(image3);
-    VenueData vdata3 = new VenueData(imagelist2,'TL','003',DateTime.now(),DateTime.now(),'Chicago downtown', 'Chicago', 'City break', '\$\$', 'vibrant');
+    VenueData vdata3 = new VenueData(imagelist3,'TL','003',DateTime.now(),DateTime.now(),'Chicago downtown', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+    // contruct vdata4
+    List<Image> imagelist4 = new  List<Image>();
+    File file4 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image4 = Image.file(file4); 
+    imagelist3.add(image4);
+    VenueData vdata4 = new VenueData(imagelist4,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 2', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+    // contruct vdata5
+    List<Image> imagelist5 = new  List<Image>();
+    File file5 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image5 = Image.file(file5); 
+    imagelist3.add(image5);
+    VenueData vdata5 = new VenueData(imagelist5,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 3', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+    // contruct vdata6
+    List<Image> imagelist6 = new  List<Image>();
+    File file6 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image6 = Image.file(file6); 
+    imagelist3.add(image6);
+    VenueData vdata6 = new VenueData(imagelist6,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 4', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+  // contruct vdata7
+    List<Image> imagelist7 = new  List<Image>();
+    File file7 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image7 = Image.file(file7); 
+    imagelist3.add(image7);
+    VenueData vdata7 = new VenueData(imagelist7,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 5', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+    // contruct vdata8
+    List<Image> imagelist8 = new  List<Image>();
+    File file8 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image8 = Image.file(file8); 
+    imagelist3.add(image8);
+    VenueData vdata8 = new VenueData(imagelist8,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 6', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+ // contruct vdata9
+    List<Image> imagelist9 = new  List<Image>();
+    File file9 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image9 = Image.file(file9); 
+    imagelist3.add(image9);
+    VenueData vdata9 = new VenueData(imagelist9,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 7', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+    // contruct vdata10
+    List<Image> imagelist10 = new  List<Image>();
+    File file10 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image10 = Image.file(file10); 
+    imagelist3.add(image10);
+    VenueData vdata10 = new VenueData(imagelist10,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 8', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+ // contruct vdata11
+    List<Image> imagelist11 = new  List<Image>();
+    File file11 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image11 = Image.file(file11); 
+    imagelist3.add(image11);
+    VenueData vdata11 = new VenueData(imagelist11,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 9', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+    // contruct vdata12
+    List<Image> imagelist12 = new  List<Image>();
+    File file12 = new File('/data/user/0/com.trueconnect.launch/cache/IMG_1596943769594.png');
+    Image image12 = Image.file(file12); 
+    imagelist3.add(image12);
+    VenueData vdata12 = new VenueData(imagelist12,'TL','004',DateTime.now(),DateTime.now(),'Chicago downtown 10', 'Chicago', 'City break', '\$\$', 'vibrant');
+
+
 
     List<VenueData> venuedataList = new List<VenueData>();
     venuedataList.add(vdata1);
     venuedataList.add(vdata2);
     venuedataList.add(vdata3);
+    venuedataList.add(vdata4);
+    venuedataList.add(vdata5);
+    venuedataList.add(vdata6);
+    venuedataList.add(vdata7);
+    venuedataList.add(vdata8);
+    venuedataList.add(vdata9);
+    venuedataList.add(vdata10);
+    venuedataList.add(vdata11);
+    venuedataList.add(vdata12);
 
-
-    VenueDataDao vdatadao = new VenueDataDao(3);
+    VenueDataDao vdatadao = new VenueDataDao(12);
     vdatadao.initialiseWithData(venuedataList);
 
-    List<VenueData> retvenuedataList = vdatadao.RetrieveVenueData(4);
-    print('Calendar page tabs : ' + retvenuedataList.toList().toString()); 
-
-
- //   cPageAll = new CalendarPageAll(venuedataList);
+    cPageAll = new CalendarPageAll(vdatadao);
 
 }
 
@@ -107,8 +176,8 @@ Widget build(BuildContext context) {
           child:
           TabBarView(
             children: [
-             //cPageAll,
-              Icon(Icons.account_box),
+             cPageAll,
+             // Icon(Icons.account_box),
               Icon(Icons.access_alarm),
               Icon(Icons.access_time),
               Icon(Icons.accessibility),
