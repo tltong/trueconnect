fc9d41a94666647cef1e279bccf8a39d4430e9f0
tltong <tltong@yahoo.co.uk>
2021-01-11 00:55:30 +0800

    user profile photo page further wip

---
+++ lib/data/simple_image_struct.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:trueconnect/utils/image_util.dart';
+
+class SimpleImageStruct {
+
+  Image image;
+  bool profile;
+
+  SimpleImageStruct(Image inImage, bool inProfile){
+    image=inImage;
+    profile=inProfile;
+  }
+
+  
+
+}
--- lib/pages/user_page/user_profile_photo_page.dart
+++ lib/pages/user_page/user_profile_photo_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trueconnect/data/simple_image_struct.dart';
 import 'package:trueconnect/utils/image_util.dart';
+
 
 
 enum photoSelection { Gallery, Facebook }
@@ -18,60 +20,94 @@ class UserProfilePhotoPage extends StatefulWidget {
   }
 
 }
+/*
+  return values
+  - SimpleImageStruct with image (choosen by user)
+  - SimpleImageStruct with no image (user chose to remove photo)
+  - null (no action taken)
+*/
+Future<SimpleImageStruct>asyncSimpleDialog(BuildContext context, SimpleImageStruct inImgStrct) async {
 
-class ImageStruct {
+  
+  if (inImgStrct.image==null){
+    print('user profile photo page : null image ');
+      // currently no image is selected, show only one option (choose image)
+    return showDialog(
+          context: context,
+          barrierDismissible: true,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: const Text('Photo options'),
+              children: <Widget>[
 
-  Image image;
-  bool profile;
+                SimpleDialogOption(
+                  onPressed: () {
+                    ImageUtil.pickImageFromGallery().then((retFile){
+                      if(retFile!=null){
+                        Image retImage = new Image.file(retFile);
+                        SimpleImageStruct ret = new SimpleImageStruct(retImage,false);
+                        print('user profile photo page. selectd image from gallery' + retImage.toString());
+                        Navigator.pop(context,ret);
+                      }
+                    });
+                  },
+                  child: const Text('Choose new photo'),
+                ),
+      ],
+          );
+        });
+  }else{
+    // current image is not empty.  show all 3 options
+        print('user profile photo page, current image is not empty.  show all 3 options');
+        return showDialog(
+          context: context,
+          barrierDismissible: true,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: const Text('Photo options'),
+              children: <Widget>[
 
-  ImageStruct(Image inImage, bool inProfile){
-    image=inImage;
-    profile=inProfile;
+                SimpleDialogOption(
+                  onPressed: () {
+                    ImageUtil.pickImageFromGallery().then((retFile){
+                       print('user profile photo page. return from image picker');
+                      if(retFile!=null){
+                        print('user profile photo page. photo selected from gallery');
+                        Image retImage = new Image.file(retFile);
+                        SimpleImageStruct ret = new SimpleImageStruct(retImage,false);
+                        print('user profile photo page. selectd image from gallery' + retImage.toString());
+                        Navigator.pop(context,ret);
+                      }
+                      print('user profile photo page. image picker exit');
+                    });
+                  },
+                  child: const Text('Choose new photo'),
+                ),
+
+                SimpleDialogOption(
+                  onPressed: () {
+                        SimpleImageStruct ret = new SimpleImageStruct(null,false);
+                        Navigator.pop(context,ret);
+                      
+                  },
+                  child: const Text('Remove photo'),
+                ),
+
+                SimpleDialogOption(
+                  onPressed: () {
+                      
+                  },
+                  child: const Text('Make profile photo'),
+                ),
+         
+      ],
+          );
+        });
+
+
   }
 
-}
 
-
-
-Future<Image>asyncSimpleDialog(BuildContext context) async {
-
-  return showDialog(
-        context: context,
-        barrierDismissible: true,
-        builder: (BuildContext context) {
-          return SimpleDialog(
-            title: const Text('Photo options'),
-            children: <Widget>[
-
-              SimpleDialogOption(
-                onPressed: () {
-
-                  ImageUtil.pickImageFromGallery().then((retFile){
-                    if(retFile!=null){
-                      print('user profile photo page before navigator pop');
-
-                      Image retImage = new Image.file(retFile);
-
-                      Navigator.pop(context,retImage);
-                      print('user profile photo page after navigator pop');
-                   //   print('user profile photo page selected image : ' + retimage.toString());
-
-//                      return retimage;
-
-                    }
-                  
-                  });
-/*
-                  print('user profile photo page before navigator pop');
-                  Navigator.pop(context);
-                  print('user profile photo page after navigator pop');
-*/
-                },
-                child: const Text('Choose new photo'),
-              ),
-     ],
-        );
-      });
 
 
 }
@@ -81,6 +117,8 @@ class UserProfilePhotoPageState extends State<UserProfilePhotoPage>  {
 
   final PageStorageBucket bucket = PageStorageBucket();
   Image image1,image2,image3;
+  Container container1, container2, container3;
+  var display1,display2,display3;
 
   @override
     void initState() {
@@ -93,10 +131,81 @@ class UserProfilePhotoPageState extends State<UserProfilePhotoPage>  {
       super.dispose();
     }
 
+
+Container profileFrameFromImage (Image inImage){
+
+  var retimage;
+  switch(ImageUtil.Imagetype(inImage)){
+
+    case 'NetworkImage':
+      retimage = new NetworkImage(ImageUtil.ExtractImageString(inImage));
+      return new Container(
+        decoration: new BoxDecoration(
+          border: new Border.all(
+            color: Colors.green,
+            width: 3.0,
+            style: BorderStyle.solid
+          ),
+          image: new DecorationImage(
+              image: retimage,
+          )
+        ));
+    break;
+
+    case 'FileImage':
+      retimage = new FileImage(ImageUtil.FileFromImage(inImage));
+       return new Container(
+        decoration: new BoxDecoration(
+          border: new Border.all(
+            color: Colors.green,
+            width: 3.0,
+            style: BorderStyle.solid
+          ),
+          image: new DecorationImage(
+              image: retimage,
+          )
+        ));
+    break;
+  }
+}
+
+Container normalFrameFromImage (Image inImage){
+
+  var retimage;
+  print('user profile photo page : image : ' + inImage.toString());
+  
+  switch(ImageUtil.Imagetype(inImage)){
+
+    case 'NetworkImage':
+      print('user profile photo page : network image' );
+          
+      retimage = new NetworkImage(ImageUtil.ExtractImageString(inImage));
+      return new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+              image: retimage,
+          )
+        ));
+    break;
+
+    case 'FileImage':
+      print('user profile photo page : file image' );
+
+      retimage = new FileImage(ImageUtil.FileFromImage(inImage));
+       return new Container(
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+              image: retimage,
+          )
+        ));
+    break;
+  }
+}
+
+
+
   @override
    Widget build(BuildContext context) {
-
-   
 
    return  
     
@@ -109,19 +218,37 @@ class UserProfilePhotoPageState extends State<UserProfilePhotoPage>  {
         crossAxisCount: 3,
         children: <Widget>[
      
-            GestureDetector(
+           GestureDetector(
 
-           child: image1==null?Image.asset('images/addphoto.png'):image1,
-            
+           child: display1==null? Image.asset('images/addphoto.png'):display1,
+          
             onTap: () async {
-              print('user profile photo page : before selection ');
 
-               asyncSimpleDialog(context).then((image) async {
-                print('user profile photo page : after selection ' + image.toString());
+            // ***** TO DO - to extrat image from container *******
 
-                setState((){
-                  image1=image;
-                 });
+              print('user profile photo page. display1 image value ' + display1.child);
+            
+              SimpleImageStruct imgstrct = new SimpleImageStruct(display1.child,true);
+
+
+               asyncSimpleDialog(context,imgstrct).then((ret) async {
+                
+                if (ret!=null){   // do nothing if null
+                  setState((){
+                   print('user profile photo page. returned image ' + ret.image.toString());
+                                      
+                   if (ret.profile==true){
+                      print('user profile photo page : profile photo' );
+                      display1=profileFrameFromImage(ret.image);
+                    }else{
+                      print('user profile photo page : non profile photo' );
+                      display1=normalFrameFromImage(ret.image);
+                    }
+                 }
+                
+                );
+                }
+                
 
                               });
             
